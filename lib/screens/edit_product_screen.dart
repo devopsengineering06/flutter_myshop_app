32d939a9309e5c3294384237a9187cf665b5782a
devop <devops.engineering06@gmail.com>
2022-12-26 17:22:11 +0300

    Validating User Input

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -49,6 +49,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
   }
 
   void _saveForm() {
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      return;
+    }
     _formKey.currentState!.save();
     debugPrint(_editedProduct.title);
     debugPrint(_editedProduct.description);
@@ -79,6 +83,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please provide a value';
+                  }
+                  return null;
                 },
                 onSaved: (value) {
                   _editedProduct = Product(
