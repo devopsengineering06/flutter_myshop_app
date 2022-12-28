fadbc1bb1fbb9e69b09cb4d8a0058da7bbc494f6
devop <devops.engineering06@gmail.com>
2022-12-28 21:27:19 +0300

    Adding User Signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../screens/auth_screen.dart';
-import '../screens/edit_product_screen.dart';
-import '../screens/user_products_screen.dart';
-import '../screens/orders_screen.dart';
-import '../providers/orders.dart';
-import '../screens/cart_screen.dart';
+import './providers/auth.dart';
+import './screens/auth_screen.dart';
+import './screens/edit_product_screen.dart';
+import './screens/user_products_screen.dart';
+import './screens/orders_screen.dart';
+import './providers/orders.dart';
+import './screens/cart_screen.dart';
 import './providers/cart.dart';
-import '../providers/products.dart';
-import '../screens/product_detail_screen.dart';
+import './providers/products.dart';
+import './screens/product_detail_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -20,6 +21,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (context) => Auth(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
---
+++ lib/providers/auth.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: unused_field
+
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  late String _token;
+  late DateTime _expiryDate;
+  late String _userId;
+
+  Auth();
+
+  Future<void> signup(String email, String password) async {
+    String authId = 'AIzaSyCiAX461bCWh0qTCDHLz9G9Kph91Vzncgo';
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$authId');
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    // ignore: avoid_print
+    print(json.decode(response.body));
+  }
+}
